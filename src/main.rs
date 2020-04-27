be1012d05f334874ffc1e24761f2881386aee771
Linden Chiu <hchiu87@gmail.com>
2020-04-26 22:57:21 -0700

    working static page, hot reload

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
-//use actix_files as fs;
+use actix_files as fs;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
 
-async fn index(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+
+// async fn index(_req: HttpRequest) -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let mut listenfd = ListenFd::from_env();
+
+    let mut server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(index))
             .service(
-              web::scope("/app").route("/index.html", web::get().to(index)),
+                 // static files
+                 fs::Files::new("/", "./static/").index_file("index.html"),
             )
-    })
-    .bind("127.0.0.1:8088")?
-    .run()
-    .await
+    });
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:8088")?
+    };
+
+    server.run().await
+   
 }
+
+
